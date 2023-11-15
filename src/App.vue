de6e14b8cbd5a9accea3266ba03929126f9df94f
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-11-15 16:56:58 +0100

    Add - menù dinamico in AppHeader.vue

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,28 @@ export default {
     AppHeader,
     AppMain,
     AppFooter
+  },
+  data() {
+    return {
+      logo: { src: './public/HelloIMG1669149314369.png', alt: 'Logo', text: '' },
+      menuItems: [
+        { text: 'Home', link: '#' },
+        { text: 'Mission', link: '#' },
+        { text: 'Causes', link: '#' },
+        { text: 'Journal', link: '#' },
+        { text: 'Donate', link: '#' }
+      ]
+    };
   }
 }
 </script>
 
 <template>
-  <AppHeader />
+  <AppHeader :logo="logo" :menu-items="menuItems" />
   <AppMain />
   <AppFooter />
 </template>
 
 <style lang="scss">
-@use './styles/general.scss' as*;
+@use './styles/general.scss' as*
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,3 +1,65 @@
-<script></script>
-<template></template>
-<style></style>
+<script>
+
+export default {
+    name: 'AppHeader',
+    props: ['logo', 'menuItems'],
+    mounted() {
+        console.log('menuItems:', this.menuItems);
+    }
+};
+
+</script>
+<template>
+    <header>
+        <div class="header-container">
+            <div class="logo">
+                <img :src="logo.src" :alt="logo.alt">
+                <span>{{ logo.text }}</span>
+            </div>
+            <nav>
+                <ul>
+                    <li v-for="(menuItem, index) in menuItems" :key="index">
+                        <a :href="menuItem.link">{{ menuItem.text }}</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+<style lang="scss" scoped>
+.header-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+}
+
+.logo {
+    display: flex;
+    align-items: center;
+}
+
+.logo img {
+    max-width: 150px;
+    height: auto;
+    margin-right: 10px;
+    margin-left: 50px;
+
+}
+
+nav ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+}
+
+nav li {
+    margin-right: 50px;
+}
+
+a {
+    text-decoration: none;
+    color: #000;
+}
+</style>
